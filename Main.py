5b6b661329883249da002003ad6ea32c5335a8a8
AdamSedla <77066378+AdamSedla@users.noreply.github.com>
2021-12-27 13:13:01 +0100

    Added printing whole daymenu

--- Main.py
+++ Main.py
@@ -1,8 +1,26 @@
 import pandas as pd
-
 sheet_code = "1kq9QMXmexts_Yqor7Jqhmo0IpU8BKZluSGWvrjNXcvU"
 sheet_url = "https://docs.google.com/spreadsheets/d/"+sheet_code+"/export?format=csv"
 
 x = pd.read_csv(sheet_url).to_dict()
 
-print (x)
+class ScholarestMenu:
+    def __init__(self, Soup, Menu1, Menu2, Menu3):
+        self.Soup = Soup
+        self.Menu1 = Menu1
+        self.Menu2 = Menu2
+        self.Menu3 = Menu3
+
+#Variables
+RowDifferences = 7
+MenuRow = 0
+######
+
+
+
+Monday = ScholarestMenu(x["Unnamed: 1"][MenuRow], x["Unnamed: 1"][MenuRow + 2], x["Unnamed: 1"][MenuRow + 3], x["Unnamed: 1"][MenuRow + 4])
+
+print(Monday.Soup)
+print(Monday.Menu1)
+print(Monday.Menu2)
+print(Monday.Menu3)
