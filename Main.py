3626c77a250ffe5172eb68f43c578e1d48fd81e1
AdamSedla <77066378+AdamSedla@users.noreply.github.com>
2021-12-27 14:15:06 +0100

    Added user input

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 sheet_code = "1kq9QMXmexts_Yqor7Jqhmo0IpU8BKZluSGWvrjNXcvU"
 sheet_url = "https://docs.google.com/spreadsheets/d/"+sheet_code+"/export?format=csv"
+Week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+RowDifferences = 7
+MenuRow = 0
 
 x = pd.read_csv(sheet_url).to_dict()
 
@@ -11,16 +14,16 @@ class ScholarestMenu:
         self.Menu2 = Menu2
         self.Menu3 = Menu3
 
-#Variables
-RowDifferences = 7
-MenuRow = 0
-######
+dayInput =  str(input("day of the week?"))
 
+day = Week.index(dayInput)
 
+try:
+    Monday = ScholarestMenu(x["Unnamed: 1"][MenuRow + RowDifferences*day], x["Unnamed: 1"][MenuRow + 2 + RowDifferences*day], x["Unnamed: 1"][MenuRow + 3 + RowDifferences*day], x["Unnamed: 1"][MenuRow + 4 + RowDifferences*day])
+    print(Monday.Soup)
+    print(Monday.Menu1)
+    print(Monday.Menu2)
+    print(Monday.Menu3)
 
-Monday = ScholarestMenu(x["Unnamed: 1"][MenuRow], x["Unnamed: 1"][MenuRow + 2], x["Unnamed: 1"][MenuRow + 3], x["Unnamed: 1"][MenuRow + 4])
-
-print(Monday.Soup)
-print(Monday.Menu1)
-print(Monday.Menu2)
-print(Monday.Menu3)
+except:
+    print("No food for U today, sorry")
