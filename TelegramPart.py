de8834b1dd576ef72231e685039b82f68eb8d62a
AdamSedla <77066378+AdamSedla@users.noreply.github.com>
2022-01-27 10:06:39 +0100

    fix

--- TelegramPart.py
+++ TelegramPart.py
@@ -9,7 +9,7 @@ Message = "Menu pro dnešek:" + "\n\n"
 def telegram_bot_sendtext(bot_message):
 
     bot_token = Codes.Token
-    bot_chatID = Codes.ChatID_private
+    bot_chatID = Codes.ChatID
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
