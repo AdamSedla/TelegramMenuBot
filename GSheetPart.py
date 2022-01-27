99596b9fc3e7e5b0b62da8731d6a7f36c532db46
AdamSedla <77066378+AdamSedla@users.noreply.github.com>
2022-01-27 11:37:14 +0100

    Changed names in Dicts

--- GSheetPart.py
+++ GSheetPart.py
@@ -10,38 +10,38 @@ FirstLane = 4
 DayDifference = 7
 
 Monday = {  "Polévka:":  x["Unnamed: 2"][FirstLane],
-            "Dobrůtka:": x["Unnamed: 2"][FirstLane + 1],
-            "*1:": x["Unnamed: 2"][FirstLane + 2],
-            "*2:": x["Unnamed: 2"][FirstLane + 3],
-            "*3:": x["Unnamed: 2"][FirstLane + 4]
+            "Ňamka:": x["Unnamed: 2"][FirstLane + 1],
+            "Oběd 1:": x["Unnamed: 2"][FirstLane + 2],
+            "Oběd 2:": x["Unnamed: 2"][FirstLane + 3],
+            "Oběd 3:": x["Unnamed: 2"][FirstLane + 4]
 }
 
 Tuesday = { "Polévka:":  x["Unnamed: 2"][FirstLane + DayDifference],
-            "Dobrůtka:": x["Unnamed: 2"][FirstLane + DayDifference + 1],
-            "*1:": x["Unnamed: 2"][FirstLane + DayDifference + 2],
-            "*2:": x["Unnamed: 2"][FirstLane + DayDifference + 3],
-            "*3:": x["Unnamed: 2"][FirstLane + DayDifference + 4]
+            "Ňamka:": x["Unnamed: 2"][FirstLane + DayDifference + 1],
+            "Oběd 1:": x["Unnamed: 2"][FirstLane + DayDifference + 2],
+            "Oběd 2:": x["Unnamed: 2"][FirstLane + DayDifference + 3],
+            "Oběd 3:": x["Unnamed: 2"][FirstLane + DayDifference + 4]
 }
 
 Wednesday = { "Polévka:":x["Unnamed: 2"][FirstLane + 2 * DayDifference],
-              "Dobrůtka:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 1],
-              "*1:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 2],
-              "*2:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 3],
-              "*3:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 4]
+              "Ňamka:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 1],
+              "Oběd 1:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 2],
+              "Oběd 2:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 3],
+              "Oběd 3:": x["Unnamed: 2"][FirstLane + 2 * DayDifference + 4]
 }
 
 Thursday = { "Polévka:": x["Unnamed: 2"][FirstLane + 3 * DayDifference],
-             "Dobrůtka:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 1],
-             "*1:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 2],
-             "*2:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 3],
-             "*3:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 4]
+             "Ňamka:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 1],
+             "Oběd 1:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 2],
+             "Oběd 2:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 3],
+             "Oběd 3:": x["Unnamed: 2"][FirstLane + 3 * DayDifference + 4]
 }
 
 Friday = {  "Polévka:":  x["Unnamed: 2"][FirstLane + 4 * DayDifference],
-            "Dobrůtka:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 1],
-            "*1:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 2],
-            "*2:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 3],
-            "*3:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 4]
+            "Ňamka:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 1],
+            "Oběd 1:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 2],
+            "Oběd 2:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 3],
+            "Oběd 3:": x["Unnamed: 2"][FirstLane + 4 * DayDifference + 4]
 }
 
 def ChooseDay():
