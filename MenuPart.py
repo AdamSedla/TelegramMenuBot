0a2efe856dbc8b3f22c6e0d20da91b4ddcde0639
AdamSedla <77066378+AdamSedla@users.noreply.github.com>
2022-01-31 16:55:46 +0100

    added Function DayMenu(day)

--- MenuPart.py
+++ MenuPart.py
@@ -6,22 +6,25 @@ sheet_url = "https://docs.google.com/spreadsheets/d/"+Codes.GSheet+"/export?form
 
 x = pandas.read_csv(sheet_url).to_dict()
 
-FirstLane = 4
-DayDifference = 7
-
 day = datetime.datetime.today().weekday()
 
-Menu = { "Polévka:": x["Unnamed: 2"][FirstLane + DayDifference * day],
-         "Ňamka:":   x["Unnamed: 2"][FirstLane + DayDifference * day + 1],
-         "Oběd 1:":  x["Unnamed: 2"][FirstLane + DayDifference * day + 2],
-         "Oběd 2:":  x["Unnamed: 2"][FirstLane + DayDifference * day + 3],
-         "Oběd 3:":  x["Unnamed: 2"][FirstLane + DayDifference * day + 4]
-}
+week = ["Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek"]
 
-Message = "Menu pro dnešek:" + "\n\n"
+def DayMenu(day):
+    FirstLane = 4
+    DayDifference = 7
 
-try:
-    for key, value in Menu.items():
-        Message += key + "\n" + value + "\n\n"
-except:
-    Message = "Error 666" + "\n" + "Dnes bude karbanátek po Ukrajinsku podle strýčka Stalina"
+    Menu = { "Polévka:": x["Unnamed: 2"][FirstLane + DayDifference * day],
+            "Ňamka:":   x["Unnamed: 2"][FirstLane + DayDifference * day + 1],
+            "Oběd 1:":  x["Unnamed: 2"][FirstLane + DayDifference * day + 2],
+            "Oběd 2:":  x["Unnamed: 2"][FirstLane + DayDifference * day + 3],
+            "Oběd 3:":  x["Unnamed: 2"][FirstLane + DayDifference * day + 4]
+    }
+    Message = "Menu pro " + week[day] + ":" + "\n\n"
+    try:
+        for key, value in Menu.items():
+            Message += key + "\n" + value + "\n\n"
+    except:
+        Message = "Error 666" + "\n" + "Dnes bude karbanátek po Ukrajinsku podle strýčka Stalina"
+
+    return Message
