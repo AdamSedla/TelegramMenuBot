18483ea8a32528b86501588200819709e52a4cae
Adam Sedláček <xsedad01@gjk.cz>
2022-02-25 14:18:34 +0100

    added error messages

--- DailyMenu.py
+++ DailyMenu.py
@@ -4,11 +4,13 @@ import telebot
 
 bot = telebot.TeleBot(Codes.Token)
 
-bot.unpin_all_chat_messages(Codes.ChatID)
-Message = bot.send_message(
-            Codes.ChatID,
-            MenuPart.DayMenu(2)
-            )
-MessageID = Message.message_id
-
-bot.pin_chat_message(Codes.ChatID, MessageID)
+try:
+    bot.unpin_all_chat_messages(Codes.ChatID)
+    Message = bot.send_message(
+                Codes.ChatID,
+                MenuPart.DayMenu(2)
+                )
+    MessageID = Message.message_id
+    bot.pin_chat_message(Codes.ChatID, MessageID)
+except:
+    print("Error in DailyMenu.py")
